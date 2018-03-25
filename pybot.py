469bbcebf1a7f8df77c6389319873532ce71673d
el3phanten <bogisivertsen@gmail.com>
2018-03-26 00:34:22 +0200

    Update pybot.py

--- pybot.py
+++ pybot.py
@@ -12,7 +12,7 @@ irc.connect((server, 6667))
 irc.send(bytes("USER {user} {user} {user} :This is a fun bot!\r\n".format(user=botnick),"utf8"))
 irc.send(bytes("NICK {}\r\n".format(botnick),"utf8"))
 irc.send(bytes("PRIVMSG nickserv :iNOOPE\r\n","utf8"))
-irc.send(bytes("PRIVMSG nickserv :identify {}\r\n".format(sys.argv[3]),"utf8"))
+irc.send(bytes("PRIVMSG nickserv :identify {}\r\n".format(sys.argv[2]),"utf8"))
 sys.argv[3]=0
 time.sleep(20)
 irc.send(bytearray("JOIN {}\r\n".format(channels[0]),"utf8"))
