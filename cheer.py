2b9d08167b32b155bb7123823e299c3c5504df53
el3phanten <bogisivertsen@gmail.com>
2018-03-27 22:17:43 +0200

    Delete cheer.py

--- cheer.py
+++
@@ -1,3 +0,0 @@
-def cheer(t='Usage: cheer("str",n=color,C=[color,...],s=star,p=padding)',n=0,C=3*[0],s="*",p=" "): print( "".join(map(lambda u,s=s,p=p: "%02d"%(u%16)+p+s, C))+"  %02d%s "%(n,t)+"".join(map(lambda u,s=s,p=p: "%02d"%(u%16)+p+s,C[::-1])))
-
-def cheer2(t='Usage: cheer("str",n=color,C=[color,...],s=star,p=padding)',n=0,C=3*[0],s="*",p=" ",f=(lambda c,)
