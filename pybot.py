457f4097abb9cc091f3785cc3610ae069f14cd0d
el3phanten <bogisivertsen@gmail.com>
2018-04-03 18:44:53 +0200

    Update pybot.py

--- pybot.py
+++ pybot.py
@@ -21,7 +21,7 @@ for i in channels:
     irc.send(bytearray("JOIN {}\r\n".format(i),"utf8"))
 
     
-class timeout:
+class Timeout:
     def __init__(self, seconds=1, error_message='Timeout'):
         self.seconds = seconds
         self.error_message = error_message
